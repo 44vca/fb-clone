aca3665fe2c61eabc9e1361e3655f1bbf8c453eb
Tonight Dark <aleksandr.abdurazakov@gmail.com>
2020-09-02 13:57:21 +0300

    implement widget in Feed Component and implement authorization page

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 import Header from './Header/Header';
 import Sidebar from './Sidebar/Sidebar';
 import Feed from './Feed/Feed';
+import Widget from './Feed/Widget/Widget';
+import Login from './Login/Login';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
 
-      <Header />
-
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-      </div>
-        
+      {!user ? (
+        <Login />
+      ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Feed />
+              <Widget />
+            </div>
+            </>
+        )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAA7fBCCEyBTAfIR3Q6iDNW_zPaceNGqVw",
     authDomain: "facebook-clone-2e8ce.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:475009077736:web:e17295ea56202cea1f545d",
     measurementId: "G-XM722QLX6G"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
